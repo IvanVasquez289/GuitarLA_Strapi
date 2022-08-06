7cef0df1596350965a0d8b6abc4ce057b0c1262b
Ivan Vasquez <ivanjv1234@gmail.com>
2022-08-06 01:18:29 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c8bd1fa55233b0653804a0b1e245818'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9746484e-78fb-4f05-987f-e2b07ad63b13'
+};
